10ef260f25b16309f0acf2edc671abe2bd27a5da
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-01-26 20:11:11 +0300

    add ResultDTO.java and CalculationDTO.java

---
+++ inspect/src/main/java/app/dto/CalculationDTO.java
@@ -0,0 +1,11 @@
+package app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CalculationDTO {
+    private int firstOperand;
+    private int secondOperand;
+}
---
+++ inspect/src/main/java/app/dto/ResultDTO.java
@@ -0,0 +1,10 @@
+package app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResultDTO {
+    private int result;
+}
