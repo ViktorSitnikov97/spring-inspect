ec82a35f5cca7f1c73258d67081b8f80c8cc158a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-01-27 20:37:06 +0300

    add Calculator.java and CalculatorImpl.java

---
+++ inspect/src/main/java/app/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package app.calculator;
+
+public interface Calculator {
+    int sum(int a, int b);
+    int mult(int a, int b);
+}
---
+++ inspect/src/main/java/app/calculator/CalculatorImpl.java
@@ -0,0 +1,16 @@
+package app.calculator;
+
+import app.Inspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Inspect(level = "info")
+public class CalculatorImpl implements  Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+}
